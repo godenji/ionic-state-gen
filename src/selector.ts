180d2044cd0720ad6b7dc0c309ccc6881b57f34b
godenji <admin@ushr.com>
2021-09-07 16:05:44 -0700

    add generator for ngrx selector

---
+++ src/selector.ts
@@ -0,0 +1,10 @@
+import { {Entity} } from '../model/{entity}'
+import { {Key} } from '{KeyPath}'
+import { selector, {Entity}State } from '../reducer/{entity}'
+import { Selector } from 'ionic-state'
+
+const o = new Selector<{Entity}, {Key}, {Entity}State>('{entity}', selector)
+
+export const select{Entity}ById = o.byId
+export const paginated = o.paginated
+
